d015bffd34753723967c7788a0d1c4065eaf984c
Adrian Smith <aussieade@gmail.com>
2020-03-06 15:49:11 +1100

    initial framework
    Change-Id: I1d6ebbcc34e87f24bfa508ab9cdd06f436d7b2f0

---
+++ nectar_osc/freshdesk.py
@@ -0,0 +1,53 @@
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+import sys
+
+try:
+    from freshdesk.v2 import api
+except ImportError:
+    api = None
+
+from oslo_config import cfg
+
+
+CONF = cfg.CONF
+
+
+def get_client():
+    if not api:
+        print("To use this tool, you will need to also install the"
+              "python-freshdesk package: \n"
+              "  $ pip install python-freshdesk")
+        sys.exit(1)
+
+    msg = '\n'.join([
+        'No Freshdesk details found in your config file.',
+        '',
+        'To find your Freshdesk API key by following the guide here:',
+        'https://support.freshdesk.com/support/solutions/'
+        'articles/215517-how-to-find-your-api-key',
+        '',
+        'Then add the following config to your configuration',
+        'file (~/.nectar-osc.conf):',
+        '',
+        '  [freshdesk]',
+        '  api_key = <your api key>',
+        '  email_config_id = <id>',
+        '  group_id = <id>',
+        '  domain = <domainname>',
+    ])
+    if not CONF.freshdesk.domain and not CONF.freshdesk.api_key:
+        print(msg)
+        sys.exit(1)
+    return api.API(CONF.freshdesk.domain, CONF.freshdesk.api_key)
