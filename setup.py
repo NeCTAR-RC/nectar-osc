361632211065409b4ddbbf9dcfc118e195753b8f
Adrian Smith <aussieade@gmail.com>
2020-04-06 13:54:08 +1000

    add hivemind show functions
    Change-Id: I7521bf07084c9c90440e88558b0a72932d8e2c8b

--- setup.py
+++ setup.py
@@ -34,6 +34,8 @@ entry_points = {
         'nectar security instance lock = nectar_osc.security:LockInstance',
         'nectar security instance unlock = nectar_osc.security:UnlockInstance',
         'nectar security instance delete = nectar_osc.security:DeleteInstance',
+        'nectar instance show = nectar_osc.show:ShowInstance',
+        'nectar instance securitygroups = nectar_osc.show:ShowSecuritygroups',
     ]
 }
 
